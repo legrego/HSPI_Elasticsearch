8a1c9e22803217aedb98437ddf21068c44cf2944
Larry Gregory <lgregorydev@gmail.com>
2018-05-06 15:58:05 -0400

    Support publishing events by type

--- Constants.cs
+++ Constants.cs
@@ -2,9 +2,10 @@
 
 namespace HSPI_Elasticsearch
 {
-    static public class Constants
-    {
-        public static String PLUGIN_STRING_ID = "HS3_Elasticsearch";
-        public static String PLUGIN_STRING_NAME = "Elasticsearch";
+	static public class Constants
+	{
+		public static String PLUGIN_STRING_ID = "HS3_Elasticsearch";
+		public static String PLUGIN_STRING_NAME = "Elasticsearch";
+		public static String PLUGIN_DB_FILE_NAME = string.Format("{0}.sqlite", PLUGIN_STRING_NAME);
     }
 }
---
+++ Settings/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HSPI_Elasticsearch.Settings
+{
+	public class AppSettings
+	{
+		public List<EventTypeSetting> EventTypeSettings { get; set; }
+
+		public AppSettings()
+		{
+			this.EventTypeSettings = new List<EventTypeSetting>();
+		}
+
+		public bool IsEventTypeEnabled(int eventTypeId)
+		{
+			return this.EventTypeSettings.Exists(s => s.EventType.EventTypeId == eventTypeId && s.Enabled);
+		}
+	}
+
+	public class EventTypeSetting
+	{
+		public EventType EventType { get; set; }
+		public bool Enabled { get; set; }
+	}
+}
